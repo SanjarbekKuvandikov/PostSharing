14dc15f0bdfd21e652c17a287fd3e8621a7a4538
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-19 21:28:17 +0500

    Initial commit

---
+++ src/test/java/com/example/PostSharing/PostSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PostSharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
