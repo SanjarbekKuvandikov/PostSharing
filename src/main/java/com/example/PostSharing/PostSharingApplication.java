593177845bbd26aa0b9bb9ecf8f656a402e1e4ee
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-20 11:54:06 +0500

    the main class

---
+++ src/main/java/com/example/PostSharing/PostSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.PostSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostSharingApplication.class, args);
+	}
+
+}
