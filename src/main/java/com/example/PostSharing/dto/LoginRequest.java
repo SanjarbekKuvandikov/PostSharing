99764934daa64bd51db96934ed4682662a62a935
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-14 20:36:33 +0500

    Added validation in SignupController and LoginController

--- src/main/java/com/example/PostSharing/dto/LoginRequest.java
+++ src/main/java/com/example/PostSharing/dto/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.example.PostSharing.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequest {
+    @NotBlank(message = "Username cannot be empty")
     private String username;
+
+    @NotBlank(message = "Password cannot be empty")
     private String password;
 }
