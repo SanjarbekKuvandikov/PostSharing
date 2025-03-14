4f29c55ac870c357e0dd3427ff342065db283652
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-14 20:39:39 +0500

    CustomUserDetailsService improved

--- src/main/java/com/example/PostSharing/service/CustomUserDetailsService.java
+++ src/main/java/com/example/PostSharing/service/CustomUserDetailsService.java
@@ -1,14 +1,11 @@
 package com.example.PostSharing.service;
 
-import com.example.PostSharing.entity.UserEntity;
 import com.example.PostSharing.repository.UserRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -18,8 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username not found with :"+username));
-        return new User(user.getUsername(),user.getPassword(), new ArrayList<>());
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
 }
