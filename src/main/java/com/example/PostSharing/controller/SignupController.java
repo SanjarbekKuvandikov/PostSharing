799106a22a251fea6c0fb594761471defe0eec02
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-14 20:37:18 +0500

    Added validation in SignupController

--- src/main/java/com/example/PostSharing/controller/SignupController.java
+++ src/main/java/com/example/PostSharing/controller/SignupController.java
@@ -1,18 +1,21 @@
 package com.example.PostSharing.controller;
 
+import com.example.PostSharing.data.Role;
 import com.example.PostSharing.dto.SignupRequest;
 import com.example.PostSharing.entity.UserEntity;
 import com.example.PostSharing.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class SignupController {
 
@@ -20,17 +23,22 @@ public class SignupController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequest request) {
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest request, BindingResult bindingResult) {
         System.out.println("Signup method started!");
-        if (userRepository.existsByEmail(request.getUsername())){
-            return ResponseEntity.badRequest().body("Email already taken");
-        }
-        UserEntity user = new UserEntity();
-        user.setUsername(request.getUsername());
-        user.setEmail(request.getEmail());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
-        userRepository.save(user);
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors().get(0).getDefaultMessage());
+        }
+        if (userRepository.findByUsername(request.getUsername()).isPresent()){
+            return ResponseEntity.badRequest().body("Username already taken");
+        }
+        UserEntity admin = new UserEntity();
+        admin.setUsername(request.getUsername());
+        admin.setEmail(request.getEmail());
+        admin.setPassword(passwordEncoder.encode(request.getPassword()));
+        admin.setRole(Role.ADMIN);
+
+        userRepository.save(admin);
 
         return ResponseEntity.ok("User created");
     }
