1cb2f32771c50d9c5772195f60ca2adee128c264
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-15 21:12:57 +0500

    EnrollmentRequest created to catch up infos

---
+++ src/main/java/com/example/PostSharing/dto/EnrollmentRequest.java
@@ -0,0 +1,26 @@
+package com.example.PostSharing.dto;
+
+import com.example.PostSharing.data.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EnrollmentRequest {
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Phone number cannot be empty")
+    private String phoneNumber;
+
+    @NotNull(message = "Direction is required")
+    private String direction;
+
+    @NotNull(message = "Branch is required")
+    private String branch;
+
+    private Status status = Status.NEW;
+
+}
