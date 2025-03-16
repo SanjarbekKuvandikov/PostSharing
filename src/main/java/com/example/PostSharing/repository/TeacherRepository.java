6944d0a278773eda9ab95e41e2782c8d4dc4c2f2
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-16 21:47:18 +0500

    TeacherRepository interface created to save and get info

---
+++ src/main/java/com/example/PostSharing/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.example.PostSharing.repository;
+
+import com.example.PostSharing.entity.TeacherEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<TeacherEntity,Long> {
+}
