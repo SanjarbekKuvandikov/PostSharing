8bb068a5d4b1a250524e66e5e4587c65f0c4ea63
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-24 21:19:06 +0500

    TeacherController improved and all methods changed to service

--- src/main/java/com/example/PostSharing/controller/TeacherController.java
+++ src/main/java/com/example/PostSharing/controller/TeacherController.java
@@ -1,8 +1,7 @@
 package com.example.PostSharing.controller;
 
 import com.example.PostSharing.dto.TeacherDTO;
-import com.example.PostSharing.entity.TeacherEntity;
-import com.example.PostSharing.repository.TeacherRepository;
+import com.example.PostSharing.service.TeacherService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,82 +9,94 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/teacher")
 public class TeacherController {
-    private final TeacherRepository teacherRepository;
-    public TeacherController(TeacherRepository teacherRepository) {
-        this.teacherRepository = teacherRepository;
+    private final TeacherService teacherService;
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?>  register(@Valid @RequestBody TeacherDTO teacher) {
-        TeacherEntity teacherEntity = new TeacherEntity();
-
-        teacherEntity.setFullName(teacher.getFullName());
-        teacherEntity.setSpeciality(teacher.getSpeciality());
-        teacherEntity.setExperience(teacher.getExperience());
-        teacherEntity.setEducation(teacher.getEducation());
-        teacherEntity.setCertificates(teacher.getCertificates());
-        teacherEntity.setPhoneNumber(teacher.getPhoneNumber());
-        teacherEntity.setPhotoURL(teacher.getPhotoURL());
-        teacherEntity.setVideoURL(teacher.getVideoURL());
-
-        teacherRepository.save(teacherEntity);
-
+    public ResponseEntity<?>  register(@Valid @RequestBody TeacherDTO teacherDTO) {
+        teacherService.addTeacher(teacherDTO);
         return ResponseEntity.ok("Successfully registered");
     }
 
     @GetMapping("/{id}/get_name")
     public ResponseEntity<String>  getTeacherEntityName(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getFullName()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherName(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_speciality")
     public ResponseEntity<String>  getTeacherEntitySpeciality(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getSpeciality()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherSpeciality(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_experience")
     public ResponseEntity<String>  getTeacherEntityExperience(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getExperience()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherExperience(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_education")
     public ResponseEntity<String>  getTeacherEntityEducation(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getEducation()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherEducation(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_certificates")
     public ResponseEntity<String>  getTeacherEntityCertificates(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getCertificates()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherCertificates(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_phoneNumber")
     public ResponseEntity<String>  getTeacherEntityPhoneNumber(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getPhoneNumber()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherPhoneNumber(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_videoURL")
     public ResponseEntity<String>  getTeacherEntityVideoURL(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getVideoURL()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherVideoURL(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/get_imageURL")
     public ResponseEntity<String> getTeacherEntityPhotoURL(@PathVariable Long id) {
-        return teacherRepository.findById(id)
-                .map(teacher -> ResponseEntity.ok(teacher.getPhotoURL()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            String fullName = teacherService.getTeacherPhotoURL(id);
+            return ResponseEntity.ok(fullName);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
