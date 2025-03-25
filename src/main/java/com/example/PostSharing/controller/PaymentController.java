b7e21f9957196984c2a78e333c30290476d55694
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-25 15:06:16 +0500

    PaymentService improved and get main posts from PaymentController

--- src/main/java/com/example/PostSharing/controller/PaymentController.java
+++ src/main/java/com/example/PostSharing/controller/PaymentController.java
@@ -3,11 +3,7 @@ package com.example.PostSharing.controller;
 import com.example.PostSharing.data.Month;
 import com.example.PostSharing.dto.PaymentDTO;
 import com.example.PostSharing.dto.PaymentResponseDTO;
-import com.example.PostSharing.entity.GroupsEntity;
 import com.example.PostSharing.entity.PaymentEntity;
-import com.example.PostSharing.entity.StudentEntity;
-import com.example.PostSharing.repository.PaymentRepository;
-import com.example.PostSharing.repository.StudentRepository;
 import com.example.PostSharing.service.PaymentService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
@@ -16,49 +12,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
-    private final PaymentRepository paymentRepository;
-    private final StudentRepository studentRepository;
     private final PaymentService paymentService;
 
-    public PaymentController(PaymentRepository paymentRepository, StudentRepository studentRepository, PaymentService paymentService) {
-        this.paymentRepository = paymentRepository;
-        this.studentRepository = studentRepository;
+    public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/add_payment")
     public ResponseEntity<?> addPayment(@Valid @RequestBody PaymentDTO paymentDTO) {
-        StudentEntity student = studentRepository.findById(paymentDTO.getStudentId()).
-                orElseThrow(() -> new RuntimeException("Student not found"));
-
-        GroupsEntity group = student.getGroup();
-        if (group == null) {
-            throw new RuntimeException("Student does not have group");
-        }
-
-        PaymentEntity payment = paymentRepository.findByStudentIdAndYearAndMonth(
-                paymentDTO.getStudentId(),
-                        paymentDTO.getYear(),
-                        paymentDTO.getMonth()).
-                orElse(new PaymentEntity(student,paymentDTO.getYear(),paymentDTO.getMonth(),group.getFee()));
-
-        double totalPaid = (payment.getPaidAmount() != null ? payment.getPaidAmount() : 0) +paymentDTO.getPaidSum();
-        if (totalPaid > payment.getAmount()) {
-            throw new RuntimeException("Paid amount exceeds paid amount");
-        }
-
-        payment.setPaidAmount(totalPaid);
-        payment.setPaidDate(LocalDate.now());
-        payment.updatePaymentStatus();
-
-        paymentRepository.save(payment);
-
+        paymentService.addPayment(paymentDTO);
         return ResponseEntity.ok("Payment added successfully");
     }
 
@@ -82,11 +49,7 @@ public class PaymentController {
 
     @PostMapping("/payments/{id}/confirm")
     public ResponseEntity<?> confirmPayment(@PathVariable Long id) {
-        PaymentEntity payment = paymentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Payment not found"));
-
-        payment.confirmPayment();
-        paymentRepository.save(payment);
+        paymentService.confirmPayment(id);
 
         return ResponseEntity.ok("Payment confirmation successfully");
     }
--- src/main/java/com/example/PostSharing/service/PaymentService.java
+++ src/main/java/com/example/PostSharing/service/PaymentService.java
@@ -2,13 +2,16 @@ package com.example.PostSharing.service;
 
 import com.example.PostSharing.data.Month;
 import com.example.PostSharing.data.PaymentStatus;
+import com.example.PostSharing.dto.PaymentDTO;
 import com.example.PostSharing.dto.PaymentResponseDTO;
+import com.example.PostSharing.entity.GroupsEntity;
 import com.example.PostSharing.entity.PaymentEntity;
 import com.example.PostSharing.entity.StudentEntity;
 import com.example.PostSharing.repository.PaymentRepository;
 import com.example.PostSharing.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -21,6 +24,33 @@ public class PaymentService {
     public PaymentService(PaymentRepository paymentRepository, StudentRepository studentRepository) {
         this.paymentRepository = paymentRepository;
         this.studentRepository = studentRepository;
+    }
+
+    public void addPayment(PaymentDTO paymentDTO){
+        StudentEntity student = studentRepository.findById(paymentDTO.getStudentId()).
+                orElseThrow(() -> new RuntimeException("Student not found"));
+
+        GroupsEntity group = student.getGroup();
+        if (group == null) {
+            throw new RuntimeException("Student does not have group");
+        }
+
+        PaymentEntity payment = paymentRepository.findByStudentIdAndYearAndMonth(
+                        paymentDTO.getStudentId(),
+                        paymentDTO.getYear(),
+                        paymentDTO.getMonth()).
+                orElse(new PaymentEntity(student,paymentDTO.getYear(),paymentDTO.getMonth(),group.getFee()));
+
+        double totalPaid = (payment.getPaidAmount() != null ? payment.getPaidAmount() : 0) +paymentDTO.getPaidSum();
+        if (totalPaid > payment.getAmount()) {
+            throw new RuntimeException("Paid amount exceeds paid amount");
+        }
+
+        payment.setPaidAmount(totalPaid);
+        payment.setPaidDate(LocalDate.now());
+        payment.updatePaymentStatus();
+
+        paymentRepository.save(payment);
     }
 
     public List<PaymentResponseDTO> getPaymentsMonthly(Integer year, Month month) {
@@ -70,5 +100,13 @@ public class PaymentService {
                 .toList();
 
         return paymentRepository.saveAll(newPayments);
+    }
+
+    public void confirmPayment(Long id) {
+        PaymentEntity payment = paymentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Payment not found"));
+
+        payment.confirmPayment();
+        paymentRepository.save(payment);
     }
 }
